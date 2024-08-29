aaa0a316bfb638be691a5abb98e1d4292d35b8c1
thomas-ballo <liurongliang@balloai.com>
2024-08-29 21:21:10 +0800

    chore: add vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import tailwind from '@astrojs/tailwind'
 import sitemap from '@astrojs/sitemap'
+import vue from "@astrojs/vue";
 import node from '@astrojs/node'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(), vue({ devtools: true, appEntrypoint: "/src/pages/_app" }),],
   output: 'server',
   adapter: node({
     mode: 'standalone',
---
+++ src/components/Count.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1 class=" text-red-500">{{ count }}</h1>
+        <button @click="increment">Increment</button>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+const count = ref(0);
+const increment = () => count.value++;
+
+</script>
